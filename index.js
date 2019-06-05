ccefcc8fba642ff4779ad082faaf012433bf340e
Aaron Isaacs <aaron.isaacs.ing@gmail.com>
2019-06-04 21:53:20 -0500

    Window properties in Electron

--- index.js
+++ index.js
@@ -2,16 +2,42 @@
 
 const { app, BrowserWindow } = require('electron')
 
-
+//Imprimiendo un mensaje en consola antes de salir
 app.on('before-quit', () => {
   console.log('Saliendo...')
 })
 
+//Ejecutando ordenes cuando la aplicación está lista
 app.on('ready', () => {
-  let win = new BrowserWindow()
+  //Creando una ueva ventana
+  let win = new BrowserWindow({
+    width: 800, 
+    height: 600,
+    title: 'Hello world',
+    center: true,
+    maximizable: true,
+    show: false,
+  })
 
+  //
+  win.once('ready-to-show', () => {
+    win.show();
+  })
+
+  //Detectando el movimiento de la ventana
+  win.on('move', () => {
+    const position = win.getPosition()
+    console.log(`La posisión de la ventana es ${position}`);
+  })
+
+  //Detectando el cierre de la ventana para cerrar el aplicativo
   win.on('close', () => {
     win = null
     app.quit()
   })
+
+  // win.loadURL('https://app.giitic.com/m/app')
+  // win.loadURL('https://app.giitic.com')
+  win.loadURL('https://devdocs.io/')
+
 })
