86aab905384966a207e4e9c72acf5a74d5b4e02c
Aaron Isaacs <aaron.isaacs.ing@gmail.com>
2019-06-05 20:26:42 -0500

    Setting developer tools

--- src/index.js
+++ src/index.js
@@ -3,40 +3,41 @@
 import { app, BrowserWindow } from 'electron'
 import devtools from './devtools'
 
-//Validar si estamos en el entorno de desarrollo
-if(process.env.NODE_ENV === 'development') {
+// Validar si estamos en el entorno de desarrollo
+if (process.env.NODE_ENV === 'development') {
   devtools()
 }
 
-//Imprimiendo un mensaje en consola antes de salir
+// Imprimiendo un mensaje en consola antes de salir
 app.on('before-quit', () => {
   console.log('Saliendo...')
 })
 
-//Ejecutando ordenes cuando la aplicación está lista
+// Ejecutando ordenes cuando la aplicación está lista
 app.on('ready', () => {
-  //Creando una ueva ventana
+  // Creando una ueva ventana
   let win = new BrowserWindow({
-    width: 800, 
+    width: 800,
     height: 600,
     title: 'Hello world',
     center: true,
     maximizable: true,
-    show: false,
+    show: false
   })
 
-  //Esperar a que el contenido esté listo para mostrar
+  // Esperar a que el contenido esté listo para mostrar
   win.once('ready-to-show', () => {
-    win.show();
+    win.maximize()
+    win.show()
   })
 
-  //Detectando el movimiento de la ventana
+  // Detectando el movimiento de la ventana
   win.on('move', () => {
     const position = win.getPosition()
-    console.log(`La posisión de la ventana es ${position}`);
+    console.log(`La posisión de la ventana es ${position}`)
   })
 
-  //Detectando el cierre de la ventana para cerrar el aplicativo
+  // Detectando el cierre de la ventana para cerrar el aplicativo
   win.on('close', () => {
     win = null
     app.quit()
@@ -47,5 +48,4 @@ app.on('ready', () => {
   // win.loadURL('https://devdocs.io/')
   // win.loadURL(`file://${__dirname}/dist/index.html`)
   win.loadURL(`file://${__dirname}/renderer/index.html`)
-
 })
